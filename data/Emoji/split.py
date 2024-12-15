83986c374e49e78b5e1b44812469369e692930cb
朱文涛 <20373915@buaa.edu.cn>
2024-12-15 16:09:48 +0800

    yes

--- data/Emoji/split.py
+++ data/Emoji/split.py
@@ -7,15 +7,15 @@ output_file_test = 'test_data.txt'
 
 # 读取数据
 with open(input_file, 'r') as f:
-    data = f.readlines()
+    data = f.readlines()  # 读取每一行数据
 
 # 计算测试集大小 (25%)
 test_size = int(len(data) * 0.25)
 
 # 随机抽取 25% 的数据作为测试集
-random.shuffle(data)
-test_data = data[:test_size]
-train_data = data[test_size:]
+random.shuffle(data)  # 打乱数据
+test_data = data[:test_size]  # 抽取前 25% 的数据作为测试集
+train_data = data[test_size:]  # 剩余的 75% 作为训练集
 
 # 保存到新文件
 with open(output_file_train, 'w') as f_train:
